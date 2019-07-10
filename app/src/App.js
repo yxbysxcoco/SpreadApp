f733f41822101dcd258b214a4cb0a9f5541688c2
yxbysxcoco <liangzhipipi@163.com>
2019-07-10 15:29:50 +0800

    commit

--- app/src/App.js
+++ app/src/App.js
@@ -1,9 +1,31 @@
-import React from 'react';
+import React, { Component } from 'react';
+import {SpreadSheets, Worksheet, Column} from '@grapecity/spread-sheets-react';
+import '@grapecity/spread-sheets/styles/gc.spread.sheets.excel2016colorful.css';
 
-function App() {
-  return ( 
-    <div>hello world</div>
-  );
+
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.hostStyle =
+      {
+        left: "10px",
+        right: "10px",
+        position: "absolute",
+        textAlign: "left"
+      };
+    var self = this;
+    this.workbookInitialized = function(workbook){
+      self.spread = workbook;
+      console.log(workbook.getSheetCount());
+    }
+  };
+  render() {
+    return ( 
+      <div style={this.hostStyle}>
+        <SpreadSheets workbookInitialized={this.workbookInitialized}></SpreadSheets>
+      </div>
+    );
+  }
 }
 
 export default App;
